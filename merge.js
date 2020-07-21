970ca65ce5ff3c92ea734a80b76b380877a05975
Michael Hallett <m.hallett@gmail.com>
2020-07-21 13:46:33 -0400

    Adding merge stretch exercise

---
+++ merge.js
@@ -0,0 +1,10 @@
+const merge = (arr1, arr2) => {
+  const concatArr = arr1.concat(arr2);
+  const sortedArr = concatArr.sort((a, b) => a - b );
+  return sortedArr;
+};
+
+
+console.log(merge([ 4, 5, 6 ], [ 1, 2, 3, 4 ]), "=?", [ 1, 2, 3, 4, 4, 5, 6 ]);
+console.log(merge([ 4 ], [ 2, 5, 8 ]), "=?", [ 2, 4, 5, 8 ]);
+console.log(merge([ 1, 2, 6 ], []), "=?", [ 1, 2, 6 ]);
